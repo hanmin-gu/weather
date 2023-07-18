68b87eeef4edd08e70a7372d495df729e40d3317
mingu <mingu9968@gmail.com>
2023-07-18 09:50:10 +0900

    test:insertMemo Test, findById Test

--- src/test/java/com/example/weather/JdbcMemoRepositoyTest.java
+++ src/test/java/com/example/weather/JdbcMemoRepositoyTest.java
@@ -7,6 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 @Transactional
 public class JdbcMemoRepositoyTest {
@@ -15,8 +21,25 @@ public class JdbcMemoRepositoyTest {
 
     @Test
     void insertMemoTest(){
-        Memo newMemo = new Memo(1,"this is new memo-");
+        //when
+        Memo newMemo = new Memo(2,"this is new memo-");
+        //given
         jdbcMemoRepository.save(newMemo);
+        Optional<Memo> result = jdbcMemoRepository.findById(2);
+        //then
+        assertEquals(result.get().getText(),"this is new memo-");
     }
+    @Test
+    void findByIdTest(){
+        //when
+        List<Memo> memoList = jdbcMemoRepository.findAll();
+        //given
+
+        //then
+        assertNotNull(memoList);
+
+
+    }
+
 
 }
