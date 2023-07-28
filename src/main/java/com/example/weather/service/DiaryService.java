7ce4df04c8ef1452a5b52fb47251d4812bd904f6
mingu <mingu9968@gmail.com>
2023-07-28 21:29:36 +0900

    feat: parseWeather 추가

--- src/main/java/com/example/weather/service/DiaryService.java
+++ src/main/java/com/example/weather/service/DiaryService.java
@@ -1,13 +1,22 @@
 package com.example.weather.service;
 
+import com.fasterxml.jackson.core.*;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Service
@@ -17,12 +26,13 @@ public class DiaryService {
     private String apiKey;
 
     public void createDiary(LocalDate date, String text) {
-        String Weather = getWeatherString();
+        String weatherData = getWeatherString();
+        Map<String,Object> parseWeather = parseWeather(weatherData);
     }
 
     private String getWeatherString() {
        String apiUrl =  "https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=" + apiKey;
-
+       System.out.println(apiUrl);
        try{
            URL url = new URL(apiUrl);
            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -37,13 +47,35 @@ public class DiaryService {
            String inputLine;
            StringBuilder response = new StringBuilder();
            while((inputLine = br.readLine()) != null){
+               System.out.println(inputLine);
                response.append(inputLine);
            }br.close();
-
-           return "";
+           //System.out.println(response.toString());
+           return response.toString();
 
        }catch(Exception ex){
            return "failed to get response";
        }
     }
+
+    private Map<String, Object> parseWeather(String jsonStirng){
+        JSONParser jsonParser = new JSONParser();
+
+        JSONObject jsonObject;
+        try{
+            jsonObject =(JSONObject) jsonParser.parse(jsonStirng);
+        } catch(ParseException e){
+            throw new RuntimeException();
+        }
+        Map<String,Object> resultMap = new HashMap<>();
+        JSONObject mainData = (JSONObject) jsonObject.get("weather");
+        resultMap.put("temp", mainData.get("temp"));
+        JSONObject weatherData = (JSONObject) jsonObject.get("main");
+        resultMap.put("main", weatherData.get("main"));
+        resultMap.put("icon", weatherData.get("icon"));
+        return resultMap;
+
+
+    }
 }
+
